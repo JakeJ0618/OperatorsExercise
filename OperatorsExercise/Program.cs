99a5e59c860ed0862015fbba28440478a0d4c43b
Jake Johnston <jakejohnston618@gmail.com>
2023-03-06 13:17:08 -0600

    made math equations

---
+++ OperatorsExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Security.Cryptography.X509Certificates;
+
+int a = 6;
+int b = 37;
+
+Console.WriteLine($"Addition:{a + b}");
+Console.WriteLine();
+
+Console.WriteLine($"Subtraction:{a - b}");
+Console.WriteLine();
+
+Console.WriteLine($"Multiplication:{a * b}");
+Console.WriteLine();
+
+Console.WriteLine($"Division:{b / a}");
+Console.WriteLine();
+
+int quotient = (b / a);
+int remainder = (b % a);
+
+Console.WriteLine($"The quotient of b/a is {quotient}");
+Console.WriteLine();
+Console.WriteLine($"37/6 is {b/a} with a remainder of {remainder}");
